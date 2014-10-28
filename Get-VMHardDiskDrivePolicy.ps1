ce5f6c080ecea775afaf5d24e48c5d6e3b18915a
Andy Atkinson <aoatkinson@gmail.com>
2014-10-28 14:06:28 -0700

    Adding plang's stuff

---
+++ Get-VMHardDiskDrivePolicy.ps1
@@ -0,0 +1,75 @@
+﻿<#
+    .SYNOPSIS
+        Retrieves assigned Storage QoS PolicyID for a virtual hard disk
+
+    .DESCRIPTION
+        This returns a list of virtual hard disk drive configurations, and the 
+        assigned PolicyID. This script should only be used with Windows Server 
+        Technical Preview.
+
+    .NOTES
+        File Name : Get-VMHardDiskDrivePolicy.ps1
+        Authors   : Patrick Lang
+
+    .EXAMPLE
+        Get-VM -Name TestVm | Get-VMHardDiskDrive | .\Get-VMHardDiskDrivePolicy.ps1
+        
+        Retrieves all virtual hard disks and the policy ID assigned to each.
+
+#>
+
+param(
+    [Parameter(Mandatory=$true, 
+               ParameterSetName="ByDisk")]
+    [Microsoft.HyperV.PowerShell.HardDiskDrive]$VMHardDiskDrive,
+    [Parameter(ValueFromPipeline=$true,
+               Mandatory=$true, 
+               ValueFromPipelineByPropertyName = $true, 
+               ParameterSetName="ByPipe")]
+    [Microsoft.HyperV.PowerShell.HardDiskDrive[]]$VMHardDiskDrives
+)
+
+begin {
+
+    function GetStorageAllocationSettingData
+    {
+        param(
+        [Microsoft.HyperV.PowerShell.HardDiskDrive]$VMHardDiskDrive
+        )
+    
+        $ns = "root\virtualization\v2"
+        $svc = gwmi -n $ns Msvm_VirtualSystemManagementService
+        $instanceToQuery = $VMHardDiskDrive.Id -replace "\\", "\\" # Escape backslash for WQL query
+        $resource = gwmi -n $ns -Query "SELECT * FROM Msvm_ResourceAllocationSettingData WHERE InstanceId = '$($instanceToQuery)'"
+        if ($resource -eq $null)
+        {
+            throw "Virtual Hard Disk not found"
+        }
+        $resource.GetRelated("Msvm_StorageAllocationSettingData")
+    }
+
+}
+
+process {
+
+    switch($PSCmdlet.ParameterSetName)
+    {
+        "ByPipe" { 
+            $VMHardDiskDrives | foreach-object {
+                $sasd = GetStorageAllocationSettingData -VMHardDiskDrive $_
+                New-Object PSObject -Property @{ 
+                    VMHardDiskDrive = $_;
+                    PolicyId = $sasd.StorageQoSPolicyID
+                }
+            }
+        }
+        "ByDisk" {
+            $sasd = GetStorageAllocationSettingData -VMHardDiskDrive $VMHardDiskDrive
+            New-Object PSObject -Property @{ 
+                VMHardDiskDrive = $_;
+                PolicyId = $sasd.StorageQoSPolicyID
+            }
+        }
+    }
+
+}
---
+++ Set-VMHardDiskDrivePolicy.ps1
@@ -0,0 +1,107 @@
+﻿<#
+    .SYNOPSIS
+        Sets the Storage QoS PolicyID for a virtual hard disk
+
+    .DESCRIPTION
+        This script sets the Storage QoS PolicyID for a given virtual hard disk
+        or hard disks.  
+        
+        The New-StorageQosPolicy can create a new policy on a Scale Out File
+        Server, and the results contain the PolicyId.  Get-StorageQosPolicy
+        may also be used to query existing policies and obtain a valid PolicyID.
+        This script should only be used with Windows Server Technical Preview.
+
+    .NOTES
+        File Name : Set-VMHardDiskDrivePolicy.ps1
+        Authors   : Patrick Lang
+
+    .EXAMPLE
+        $session = New-CimSession -ComputerName TestFileCluster
+        $policy = New-StorageQosPolicy -Name TestPolicy -MinimumIOPs 100 -MaximumIOPs 500 -CimSession $session
+
+        Get-VM -Name TestVm | Get-VMHardDiskDrive | .\Set-VMHardDiskDrivePolicy.ps1 $policy.PolicyID
+        
+        Creates a new Policy 'TestPolicy' on the Scale Out File Server cluster 'TestFileCluster,'
+        and assigns it to the virtual hard disks attached to 'TestVm'
+
+#>
+
+param(
+    [Parameter(Mandatory=$true, 
+               ParameterSetName="ByDisk")]
+    [Microsoft.HyperV.PowerShell.HardDiskDrive]$VMHardDiskDrive,
+    [Parameter(ValueFromPipeline=$true,
+               Mandatory=$true, 
+               ValueFromPipelineByPropertyName = $true, 
+               ParameterSetName="ByPipe")]
+    [Microsoft.HyperV.PowerShell.HardDiskDrive[]]$VMHardDiskDrives,
+    [Parameter(ValueFromPipelineByPropertyName = $true,
+               Mandatory=$true)]
+    [string]$PolicyId = $null
+)
+
+begin {
+
+    function WaitForResult($Output)
+    {
+        if ($Output.ReturnValue -eq 4096)
+        {
+            while ($true)
+            {
+                $job = [wmi]$Output.Job
+                if ($job -ne $null)
+                {
+                    if ($job.JobState -gt 4)
+                    {
+                        write-warning $job.ErrorDescription;
+                        write-warning $job.__PATH;
+                        throw "Job returned error $($job.ErrorCode)"
+                    }
+                }
+                start-sleep 1
+            }
+        }
+    }
+    
+    function GetStorageAllocationSettingData
+    {
+        param(
+        [Microsoft.HyperV.PowerShell.HardDiskDrive]$VMHardDiskDrive
+        )
+    
+        $ns = "root\virtualization\v2"
+        $svc = gwmi -n $ns Msvm_VirtualSystemManagementService
+        $instanceToQuery = $VMHardDiskDrive.Id -replace "\\", "\\" # Escape backslash for WQL query
+        $resource = gwmi -n $ns -Query "SELECT * FROM Msvm_ResourceAllocationSettingData WHERE InstanceId = '$($instanceToQuery)'"
+        if ($resource -eq $null)
+        {
+            throw "Virtual Hard Disk not found"
+        }
+        $resource.GetRelated("Msvm_StorageAllocationSettingData")
+    }
+
+    $ns = "root\virtualization\v2"
+    $svc = gwmi -n $ns Msvm_VirtualSystemManagementService
+}
+
+process {
+    switch($PSCmdlet.ParameterSetName)
+    {
+        "ByPipe" { 
+            $sasds = $VMHardDiskDrives | foreach-object {
+                GetStorageAllocationSettingData -VMHardDiskDrive $_ 
+            }
+        }
+        "ByDisk" {
+            $sasds = GetStorageAllocationSettingData -VMHardDiskDrive $VMHardDiskDrive
+        }
+
+    }
+
+    foreach ($sasd in $sasds)
+    {
+        $sasd.StorageQoSPolicyID = $PolicyId
+        WaitForResult($svc.ModifyResourceSettings(@($sasd.GetText(2))));
+    }
+}
+
